48a5e99e61b9fa079a44f470d9d5945fed27f1f4
Shaheel <your-email@gmail.com>
2026-04-27 15:34:13 +0400

    Usage metrics update page v2

---
+++ src/pages/dashboard/Usage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Usage() {
+  return (
+    <div style={{ color: "white", padding: "40px" }}>
+      <h1>Usage Metrics Working ✅</h1>
+    </div>
+  );
+}
